4d413d5a78b2cdb3343891e3351f08f7beb04b51
fayyazfz <fayyazshaikfz@gmail.com>
2024-10-13 19:51:45 -0500

    my neew developments added

---
+++ src/test/java/com/application/Project5/Project5ApplicationTestsagain.java
@@ -0,0 +1,13 @@
+package com.application.Project5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
